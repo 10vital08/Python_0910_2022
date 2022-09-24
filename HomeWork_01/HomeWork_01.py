418bbc88d245816823925d6e3a44fd40bf4d6257
10vital08 <vital.lushnikov@mail.ru>
2022-09-24 15:18:17 +0300

    Completed task 3.1

--- HomeWork_01/HomeWork_01.py
+++ HomeWork_01/HomeWork_01.py
@@ -14,7 +14,7 @@ FindADay(day)
 
 # 2.2 Напишите программу, которая принимает на вход координаты точки (X и Y), 
 # причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится)
-
+'''
 def FindAQuarter(x, y):
     if x == 0 or y == 0:
         return 'Задаёте координаты, не равные "0"'
@@ -31,6 +31,24 @@ def FindAQuarter(x, y):
 coord_X = int(input('Задайте координату X: '))
 coord_Y = int(input('Задайте координату Y: '))
 print(FindAQuarter(coord_X, coord_Y))
+'''
 
 
+# 3.1 Напишите программу, которая по заданному номеру четверти, 
+# показывает диапазон возможных координат точек в этой четверти (x и y)
 
+def ShowRange(n):
+    if n == 1:
+        return 'x > 0 and y > 0'
+    elif n == 2:
+        return 'x < 0 and y > 0'
+    elif n == 3:
+        return 'x < 0 and y < 0'
+    elif n == 4:
+        return 'x > 0 and y < 0'
+    else:
+        return 'Такой четверти не существует'
+
+
+quarter = int(input('Введите номер четверти: '))
+print(ShowRange(quarter))
