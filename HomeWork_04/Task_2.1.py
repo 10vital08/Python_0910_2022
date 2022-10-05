4aac6f72b7a76ce162fdd8654612fe183dd04c08
10vital08 <vital.lushnikov@mail.ru>
2022-10-05 22:35:07 +0300

    Completed Task 2.1

--- HomeWork_04/Task_2.1.py
+++ HomeWork_04/Task_2.1.py
@@ -1,11 +1,17 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
-n = int(input('Введите число N'))
+n = int(input('Введите число N = '))
 prime_number = 2
-temp = 0
+temp = 2
 list_factor = []
 
 while prime_number != n:
     if n % prime_number == 0:
-        n / prime_number
+        n /= prime_number
         list_factor.append(prime_number)
+    else:
+        prime_number += 1
+if n == prime_number:
+     list_factor.append(prime_number)
+   
+print(list_factor)
