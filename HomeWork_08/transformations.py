5296cdc030b73e09eb6a61086aa32f04a1c926d4
10vital08 <vital.lushnikov@mail.ru>
2022-10-20 14:11:28 +0300

    Version 2

--- HomeWork_08/transformations.py
+++ HomeWork_08/transformations.py
@@ -2,29 +2,35 @@
 
 # преобразование информации для записи в файл
 # функция принимает список словарей
-def Convert_to_write(list_user):
-    result_string = ''
-    common_list = []
 
-    # проход по списку словарей
-    for user in list_user[:]:
-        # проход по значениям словаря
-        for key, val in user.items():
-            if val != '':
-                result_string += str(val) + '|'
-            else:
-                result_string += str(None) + '|'
-        common_list.append(result_string)
-    # print(common_list)
-    # возвращение списка строк значений словаря
-    return common_list
+
+def Convert_to_write(dict_user):
+    result_string = ''
+    string = ''
+
+    # проход по значениям словаря
+    for key, val in dict_user.items():
+        if val != '':
+            string += str(val) + '|'
+        else:
+            string += str(None) + '|'
+        # удаление разделителя в конце строки
+        result_string = string[:-1]
+
+    # возвращение строки значений словаря
+    return result_string
 
 
 # преобразование инф для поиска
-# функция принимает список строк с разделителями
-def Search_transform(list_string):
+# функция принимает строку с разделителями
+def Search_transform(string_user):
+    user_str = string_user.split('|')
 
-    return
+    # формирование словаря для поиска информации
+    dict_user = {'Фамилия': user_str[0], 'Имя': user_str[1], 'Отчество': user_str[2], 'ДР': user_str[3],
+                 'Тел': user_str[4], 'Адрес': user_str[5], 'Коммент': user_str[6]}
+
+    return dict_user
 
 
 users = []
@@ -33,7 +39,8 @@ users = []
 for user_number in range(2):
 
     # Создаём словарь для нового пользователя
-    new_user = {'age': '30', 'sex': 'Man', 'city': 'Ekaterinburg'}
+    new_user = {'Фамилия': 'Лушников', 'Имя': 'Виталий', 'Отчество': 'Владимирович', 'ДР': '10.08.1994',
+                'Тел': '927', 'Адрес': 'Пенза', 'Коммент': 'тестировщик'}
 
     # Добавляем нового пользователя в список
     users.append(new_user)
@@ -42,5 +49,5 @@ for user_number in range(2):
 # for user in users[:]:
 #     print(user)
 
-list_1 = Convert_to_write(users)
-Search_transform(list_1)
+list_1 = Convert_to_write(new_user)
+print(Search_transform(list_1))
