8fc304492d657b71c646b4d6cd32122263c15e6b
10vital08 <vital.lushnikov@mail.ru>
2022-10-27 19:00:26 +0300

    Corrected bot

--- HomeWork_09/Personal_bot/bot_commands.py
+++ HomeWork_09/Personal_bot/bot_commands.py
@@ -1,13 +1,16 @@
+from turtle import update
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import datetime
 from bot_commands import *
+from fractions import Fraction
 
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # вернуть приветствие с именем пользователя
     await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'/hi\n/time\n/help!\n/complex')
+    await update.message.reply_text(f'/hi\n/time\n/help\n/complex\n/rational')
 
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
@@ -15,10 +18,10 @@ async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def complex_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text
-    items = msg.split() # /sum 123 456
+    items = msg.split()
     complex_number_1 = complex(int(items[1]), int(items[2]))
     complex_number_2 = complex(int(items[4]), int(items[5]))
-    operation = items[3]
+    operation = items[3] # определение знака операции
 
     if operation == '+':
         string = str(complex_number_1) + ' + ' + str(complex_number_2) + ' = ' + str(complex_number_1 + complex_number_2)
@@ -33,4 +36,26 @@ async def complex_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print('Неправильно введен знак операции')
 
     await update.message.reply_text(f'{string}')
+
+
+
+async def rational_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    items = msg.split()
+    f_a = Fraction(int(items[1]), int(items[2]))
+    f_b = Fraction(int(items[4]), int(items[5]))
+    operation = items[3]
     
+    if operation == '+':
+        string = str(f_a) + ' + ' + str(f_b) + ' = ' + str(f_a + f_b)
+    elif operation == '-':
+        string = str(f_a) + ' - ' + str(f_b) + ' = ' + str(f_a-f_b)
+    elif operation == '*':
+        string = str(f_a) + ' * ' + str(f_b) + ' = ' + str(f_a*f_b)
+    elif operation == '/':
+        string = str(f_a) + ' / ' + str(f_b) + ' = ' + str(f_a/f_b)
+    else:
+        string = 'Wrong data'
+        print('Неправильно введен знак операции')
+    
+    await update.message.reply_text(f'{string}')
--- HomeWork_09/Personal_bot/main.py
+++ HomeWork_09/Personal_bot/main.py
@@ -1,16 +1,14 @@
-# from telegram import Update
-# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-# async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
-
+# Создание объекта бот
 app = ApplicationBuilder().token("5688069639:AAHOTQsWYMD_0K9GmPCpAOg0-YGaGtO0KiY").build()
 
+# Регистрация обработчиков на текстовые сообщения,
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
 app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("complex", complex_command))
+app.add_handler(CommandHandler("rational", rational_number))
 
 print('server start')
 app.run_polling()
