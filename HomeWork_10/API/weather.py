d13fe68d4a0fb666936fea2d94cfe51035cd6e9b
10vital08 <vital.lushnikov@mail.ru>
2022-11-13 16:14:15 +0300

    found cloud cover and temperature

--- HomeWork_10/API/weather.py
+++ HomeWork_10/API/weather.py
@@ -12,11 +12,8 @@ async def check_the_weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f"http://api.openweathermap.org/data/2.5/weather?q={items[1]}&appid=4321a3d417b53045aa1b6617c529c910&units=metric&lang=ru")
     a = response.json()
     print(a)
-    weather = a['weather'][0]['description']
-    print(a['weather'][0]['description'])
-    #print(a['main'][0]['temp'])
-    #print(weather)
-    #['main'][0]
-
+    
+    #облачность и температура воздуха
+    weather = a['weather'][0]['description'] + ', температура воздуха ' + str(a['main']['temp'])
+    print(type(weather))
     await update.message.reply_text(f'{weather}')
-    #await update.message.reply_text(print(a['weather'][0]['description']))
